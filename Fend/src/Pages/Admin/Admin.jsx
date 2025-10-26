9786f6498f788678fe9ab418e7167b54b5d24495
Ahmad <ahmadktib11@gmail.com>
2025-10-26 19:43:10 +0300

    seccond commit

--- Fend/src/Pages/Admin/Admin.jsx
+++ Fend/src/Pages/Admin/Admin.jsx
@@ -1,11 +1,11 @@
-import SideBar from "../../components/SideBar";
+import Sidebar from "../../components/Sidebar";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const Admin = () => {
   return (
     <div className="bg-gray-200 flex pt-16">
-      <SideBar />
+      <Sidebar />
       <div className="flex-1">
         <Outlet />
       </div>
